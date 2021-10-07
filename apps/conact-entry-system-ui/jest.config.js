75dc65878d6ed8c69993ca13b3e63719d2178e05
jrbruce86 <jrbruce86@gmail.com>
2021-10-07 18:15:46 -0400

    Convert to nx workspace

---
+++ apps/conact-entry-system-ui/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'conact-entry-system-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/conact-entry-system-ui',
+};
